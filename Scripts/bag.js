4b7e9d4e40fd5cd1cf52a4db8229e7d41adbe303
Gunjan Kuthe <gkuthe1997@gmail.com>
2022-08-25 23:21:32 +0530

    added day-3 work

--- Scripts/bag.js
+++ Scripts/bag.js
@@ -1,6 +1,6 @@
 let data = async () => {
   try {
-    let res = await fetch("https://fakestoreapi.com/products");
+    let res = await fetch("https://fakestoreapi.com/products?limit=5");
     res = await res.json();
     console.log(res)
     localStorage.setItem("bag", JSON.stringify(res));
@@ -11,20 +11,30 @@ let data = async () => {
 
 data();
 
-let bagData = JSON.parse(localStorage.getItem("bag"));
+let bagData = JSON.parse(localStorage.getItem("bag")) || [];
 console.log(bagData);
-let appendData = () => {
+let appendData = (bagData) => {
   let products = document.getElementById("products");
   products.innerHTML = null;
 
   let noOfProd = document.getElementById("prodNo");
-  noOfProd.innerText = bagData.length;
+  noOfProd.innerText = bagData.length;0
 
-  bagData.forEach(({price, title, image}) => {
+  let saved = JSON.parse(localStorage.getItem("saved")) || [];
+  document.getElementById("savedNo").innerText = saved.length; 
+
+
+  let total = 0;
+
+  bagData.forEach(({price, title, image},index) => {
+
+    total = total+price;
+    
     let div = document.createElement("div");
     div.setAttribute("id","prod")
     let divA = document.createElement("div");
     let divB = document.createElement("div");
+    divB.setAttribute("class","restore")
     let div1 = document.createElement("div");
     let div2 = document.createElement("div");
     let img = document.createElement("img");
@@ -36,7 +46,29 @@ let appendData = () => {
 
     let b1 = document.createElement("a");
     b1.innerText = "Remove";
+    b1.addEventListener("click",()=>{
+      bagData.splice(index,1);
+      localStorage.setItem("bag", JSON.stringify(bagData));
+      appendData(bagData);
+    })
     let b2 = document.createElement("a");
+    b2.addEventListener("click",()=>{
+      let ele = bagData[index];
+      
+      let count = true;
+      for(let i=0;i<saved.length;i++){
+        if(saved[i].title==title){
+          count = false;
+        }
+      }
+      if (saved.length==0 || count) {
+        saved.push(ele);
+        localStorage.setItem("saved", JSON.stringify(saved));
+      }
+      bagData.splice(index, 1);
+      localStorage.setItem("bag", JSON.stringify(bagData));
+      appendData(bagData);
+    })
     b2.innerText = "Save for later";
 
     div1.append(img);
@@ -46,6 +78,46 @@ let appendData = () => {
     div.append(divA,divB);
     products.append(div);
   });
+
+  let tax = total*0.05;
+  let shipping = 0;
+  if(total>0) shipping = 10;
+  let bill = total + shipping + tax;
+  document.getElementById("total").innerText = total;
+  document.getElementById("shipping").innerText = shipping;
+  document.getElementById("tax").innerText = tax.toFixed(2);
+  document.getElementById("bill").innerText = bill.toFixed(2);
+
+  let delivery = document.getElementById("delivery");
+  if(bill>=150){
+    delivery.style.width = "100%";
+  }else{
+    let a = (bill/150)*100;
+    delivery.style.width = `${a}%`;
+  }
 };
 
-appendData();
+appendData(bagData);
+
+
+// let cart = document.querySelector("#section>div:first-child");
+// cart.addEventListener("click",()=>{
+//   let Data = JSON.parse(localStorage.getItem("bag")) || [];
+//   appendData(Data);
+//   console.log(4);
+// })
+
+// let savedBag = document.querySelector("#section>div:last-child");
+// savedBag.addEventListener("click", () => {
+//   let Saved = JSON.parse(localStorage.getItem("saved")) || [];
+//   appendData(Saved);
+//   document.getElementById("checkout").innerHTML = null;
+//   let restore = document.querySelectorAll(".restore")
+//   restore.innerHTML = null;
+
+//   let btn = document.createElement("p");
+//   btn.innerText = "Add to Bag"
+//   restore.append(btn);
+
+//   console.log(3)
+// });
