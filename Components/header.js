a52796356ebfc2fa397d4c6fb67dacdec1e92228
Gunjan Kuthe <gkuthe1997@gmail.com>
2022-08-28 15:54:30 +0530

    added files

---
+++ Components/header.js
@@ -0,0 +1,29 @@
+let head = () => {
+  return `    <div class="topspeech">
+        <p>Free shipping over $89, or shop online and pick up select orders at a Nordstrom Rack or Nordstrom store. Learn More</p>
+    </div>
+    <nav id="top1">
+       <div id="logo"><img src="https://seeklogo.com/images/N/nordstrom-rack-logo-3AF091AD18-seeklogo.com.png" alt=""></div>
+      <div><input type="text" class="input-search" placeholder="🔍  Search for product or brands" id="searchbar"></div> 
+      <ul>
+        <div id="sign"><li><a href="#">Sign In</a>
+            <ul class="dropdown">
+                <li><button>Create Account | Sign In</button></li>
+                <li><a href="#">🛄Purchases</a></li>
+                <li><a href="#">♡Wishlist</a></li>
+                <li><a href="#">⚡️The Noddy Club</a></li>
+                <li><a href="#">🚛Shipping Address</a></li>
+                <li><a href="#">💳Payment Option</a></li>
+                <li><a href="#">☎️Contact Us</a></li>
+              </ul></li></div>
+      </ul>
+      
+       <div class="cartbtn"><a href="#">🛒</a></div>  
+       
+    </nav>
+    <div class="line">
+        <div></div>
+    </div>`;
+};
+
+export default head;
